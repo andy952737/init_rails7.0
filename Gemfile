3ab94e66c3e51540115bc591db18f9c31ecc6109
Andy Kuan <andy952737@gmail.com>
2022-11-24 13:30:10 +0800

    backend posts crud

--- Gemfile
+++ Gemfile
@@ -51,6 +51,9 @@ gem "bootsnap", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem "rspec-rails"
+  gem "factory_bot_rails"
+  gem "faker"
 end
 
 group :development do
@@ -62,6 +65,8 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+
+
 end
 
 group :test do
@@ -72,6 +77,6 @@ group :test do
 end
 
 
-gem "jquery-rails"
-gem "bootstrap"
+#gem "jquery-rails"
+#gem "bootstrap"
 gem "sass-rails"
--- app/controllers/backend/posts_controller.rb
+++ app/controllers/backend/posts_controller.rb
@@ -1,6 +1,53 @@
 class Backend::PostsController < BackendController
   
-  def index 
-  end
+    before_action :find_post, :only => [:show, :edit, :update, :destroy]
+
+    def index
+      puts "check index" 
+      puts params[:q]
+
+      @posts = Post.all
+    end
+
+    def new
+    end 
+
+    def show
+    end 
+
+    def edit
+    end
+
+    def create 
+      puts "check post" 
+      title = params[:post][:title]
+      contact = params[:post][:contact]
+
+      post_create = Post.create(params_post_permit)
+
+      redirect_to url_for(action: :index)  
+    end 
+
+    def update
+      puts "check update"
+      @Post.update(params_post_permit)
+      redirect_to url_for(action: :index)
+    end 
+
+    def destroy
+      puts "check destroy"
+      @Post.destroy
+      redirect_to url_for(action: :index)  
+    end
+
+    private 
+
+    def params_post_permit
+      params.require(:post).permit(:title, :contact)
+    end  
+   
+    def find_post
+      @Post = params[:id] ? Post.find(params[:id]) : Post.new
+    end
 
 end
