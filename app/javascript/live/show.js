c340bb29bc0f1852e7d53ade450255a76a71dab8
Andy Kuan <andy952737@gmail.com>
2022-11-30 16:49:22 +0800

    addHotspot ready

--- app/javascript/live/show.js
+++ app/javascript/live/show.js
@@ -6,22 +6,47 @@ embedpano({
 	target: "pano",                       // 挂载点id，唯一必须的参数
     html5: "always",    
 	supportHoverInfoView: true,	    			     
-	//onready: krpanoReady   
+	//onready: krpanoReady    
 	onready() {         				  // 回调函数，类似的还有onerror  
 		var krpano = document.getElementById('krpanoSWFObject');
 		krpanoReady(krpano); 
-    }      
+    }       
 }); 
 
-function clickVrImage(params, scene){
+function clickMoveVrImage(params, scene){
 	var krpano = document.getElementById('krpanoSWFObject');
 	var scene_page = scene;
 	krpano.call('to2(' + scene_page + ')');  
-	return;
+	return; 
 }
 
+function loadHotspot(hotspots){
+	//code...	
+}
+
+function addHotspot(hs_name, url, h, v, scene_page){  
+	var krpano = document.getElementById('krpanoSWFObject');
+	krpano.call('add_hotspot(' + hs_name + ',' + url + ',' + h + ',' +  v + ',' + scene_page +')');   
+    krpano.call("set(hotspot[" + hs_name + "].atv, " + h + ");");
+	krpano.call("set(hotspot[" + hs_name + "].ath, " + v + ");");   
+	krpano.call("set(hotspot[" + hs_name + "].url, " + url +");");
+	krpano.set("hotspot[" + hs_name + "].onclick", "click_hotspot_move(" + scene_page + ")");
+} 
+
 function krpanoReady(get_krpano) {  
-	var krpano = get_krpano; 
+	var krpano = get_krpano;  
+
+	/* hotpsot 值從這帶入 */    
+	var hs_name = 'hs0001' // + ((Date.now() + Math.random()) | 0); 
+   	var url = '/icon/icon_hotspot.png'; 
+    var h = -0.900;
+    var v = 56.800;    
+	var scene_page1 = "scene1";
+
+	// for & js 迴圈帶入熱點的值
+		addHotspot(hs_name, url, h, v, scene_page1);  
+
+	// krpano.call('calclength()');
 	// var test_data = document.querySelector(".click_img_a");
 	// console.log(test_data.getAttribute('data-ddd'));
 	// code ...	
